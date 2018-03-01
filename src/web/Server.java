8c6dc3902515a7dbf29c04cda9f54292a8759bf2
Hussein-Dahir <hussein_dahir@hotmail.com>
2018-03-01 21:21:40 +0200

    uploading project

---
+++ src/web/Server.java
@@ -0,0 +1,72 @@
+package web;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Server implements Runnable {
+
+	private ServerSocket server;
+	private final String webRoot;
+	private ExecutorService threadsPool;
+
+	private final int port;
+	private final int threadsLimit;
+
+
+	public static void main(String[] args) {
+		int port = 80;
+		String webRoot = "wwwroot/String.html";
+		int maxThreads = 100;
+		new Thread(new Server(port, webRoot, maxThreads)).start();
+	}
+
+	public Server(int port, String webRoot, int maxThreads) {
+		this.port = port;
+		this.threadsLimit = maxThreads;
+		this.webRoot = webRoot;
+	}
+
+	@Override
+	public void run() {
+		try {
+			server = new ServerSocket(port);
+			threadsPool = Executors.newFixedThreadPool(threadsLimit);
+		} catch (IOException e) {
+			System.err.println("Cannot listen on port " + port);
+			System.exit(1);
+		}
+		
+		System.out.println("Running server on the port " + port + 
+				" with web root folder \"" + webRoot + "\" and " + threadsLimit + " threads limit.");
+
+		while (!Thread.interrupted()) {
+			try {
+				threadsPool.execute(new Thread(new Connection(server.accept(), this)));
+			} catch (IOException e) {
+				System.err.println("Cannot accept client.");
+			}
+		}
+		close();
+	}
+
+	public void close() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			System.err.println("Error while closing server socket.");
+		}
+		threadsPool.shutdown();
+		try {
+			if (!threadsPool.awaitTermination(10, TimeUnit.SECONDS)) 
+				threadsPool.shutdownNow();
+		} catch (InterruptedException e) {}
+	}
+
+	public String getWebRoot() {
+		return webRoot;
+	}
+
+}
