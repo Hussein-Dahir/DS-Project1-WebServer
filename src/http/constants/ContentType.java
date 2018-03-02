33fb48e641acdd480e59c7d502dc2211db40ef0f
HinnHabib <habibissahinn@gmail.com>
2018-03-02 20:11:34 +0200

    Add css and javascript support

--- src/http/constants/ContentType.java
+++ src/http/constants/ContentType.java
@@ -3,4 +3,6 @@ package http.constants;
 public class ContentType {
 	public static final String TEXT = "text/plain";
 	public static final String HTML = "text/html";
+	public static final String CSS = "text/css";
+	public static final String JS = "text/javascript";
 }
