df2ab4036a565a287f40b7698942c0c3cf34a1d7
Hussein-Dahir <Hussein_Dahir@hotmail.com>
2018-03-03 14:27:27 +0200

    very minor change

--- src/http/HttpResponse.java
+++ src/http/HttpResponse.java
@@ -2,7 +2,6 @@ package http;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 import java.util.NavigableMap;
